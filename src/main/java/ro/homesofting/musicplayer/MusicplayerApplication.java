e0a76574052c6b751aebaf7d81add91f18a3f43c
Peter Zsolt <pferenczsolt@gmail.com>
2018-01-07 22:08:39 +0200

    first commit

---
+++ src/main/java/ro/homesofting/musicplayer/MusicplayerApplication.java
@@ -0,0 +1,12 @@
+package ro.homesofting.musicplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicplayerApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/homesofting/musicplayer/MusicplayerApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.homesofting.musicplayer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicplayerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
